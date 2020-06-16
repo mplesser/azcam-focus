327da1ca697d34a3a7be3b4601817689681605c2
Michael Lesser <mlesser@email.arizona.edu>
2020-06-16 13:27:00 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="azcam-focus",
-    version="20.0",
+    name="focus",
+    version="20.1",
     description="azcam plugin for focus control",
     long_description=long_description,
     author="Michael Lesser",
